168786e142df22ccd2c78bf96000b0cbbd3f58a3
0Toolz <166630318+0Toolz@users.noreply.github.com>
2024-09-02 20:27:38 -0500

    Update github_user_repo.py
    Initial code creation

---
+++ github_user_repo.py
@@ -0,0 +1,20 @@
+#	•	Endpoint: https://api.github.com/users/{username}/repos
+#	•	Method: GET
+#	•	Parameters: {username} is the GitHub username
+
+import requests
+
+username = '0Toolz'
+url = f'https://api.github.com/users/0Toolz/repos'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    repos = response.json()
+    
+    print(f"Repositories for user {username}:")
+    for repo in repos: 
+    	print(f"Name: {repo['name']}, Stars:
+    {repo['stargazers_count']}, Language: {repo['language']}, URL: {repo['html_url]}")
+    else:
+    	print(f"Failed to retrieve repositories: {response.status.code}")
