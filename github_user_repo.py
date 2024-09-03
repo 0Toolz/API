217da2604421ba6410039fe93626e2d8d212f51b
0Toolz <166630318+0Toolz@users.noreply.github.com>
2024-09-02 21:56:34 -0500

    Update github_user_repo.py

--- github_user_repo.py
+++ github_user_repo.py
@@ -15,5 +15,5 @@ if response.status_code == 200:
     print(f"Repositories for user {username}:")
     for repo in repos: 
     	print(f"Name: {repo['name']}, Stars: {repo['stargazers_count']}, Language: {repo['language']}, URL: {repo['html_url']}")
-    else:
-    	print(f"Failed to retrieve repositories: {response.status.code}") 
+else:
+    print(f"Failed to retrieve repositories: {response.status.code}") 
